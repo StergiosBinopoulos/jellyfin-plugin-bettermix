de24e462cce9cd2aa81cf92223d57bc065a7839e
Stergios Binopoulos <49957521+StergiosBinopoulos@users.noreply.github.com>
2025-07-14 21:46:39 +0300

    Remove redundant using

--- Tasks/ScanTask.cs
+++ Tasks/ScanTask.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Logging;
 using MediaBrowser.Model.Tasks;
 
 namespace Jellyfin.Plugin.BetterMix.Tasks;
