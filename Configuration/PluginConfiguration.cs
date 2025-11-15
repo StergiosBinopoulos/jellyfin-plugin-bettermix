4f7743051fbd1847e6d1239b5cd1156c5b39666c
Stergios Binopoulos <stergiosbinopoulos@gmail.com>
2025-11-15 20:21:43 +0200

    Add additional configuration fields
    - Add a noise field to each dailymix playlist
    - Add a min song length to each dailymix playlist
    - Add custom data path option to maintain data between plugin updates

--- Configuration/PluginConfiguration.cs
+++ Configuration/PluginConfiguration.cs
@@ -18,10 +18,16 @@ public class DailyMixConfig
     public required SampleMethod SampleMethod { get; set; }
     public required int InputSize { get; set; }
     public required int OutputSize { get; set; }
+    public required double Noise { get; set; }
+    public required int MinSongLength { get; set; }
 }
 
 public class PluginConfiguration : BasePluginConfiguration
 {
+    public bool CustomPathEnabled { get; set; } = false;
+    
+    public string CustomPath { get; set; } = "";
+
     public string SelectedBackend { get; set; } = "deejai";
 
     public string DeejaiMethod { get; set; } = "append";
@@ -36,35 +42,45 @@ public class PluginConfiguration : BasePluginConfiguration
             Name = "DailyMix 1",
             SampleMethod = SampleMethod.Top100,
             InputSize = 25,
-            OutputSize = 50
+            OutputSize = 50,
+            Noise = 0.01,
+            MinSongLength = 90,
         },
         new DailyMixConfig
         {
             Name = "DailyMix 2",
             SampleMethod = SampleMethod.Top200,
             InputSize = 25,
-            OutputSize = 50
+            OutputSize = 50,
+            Noise = 0.01,
+            MinSongLength = 90,
         },
         new DailyMixConfig
         {
             Name = "DailyMix 3",
             SampleMethod = SampleMethod.Top50,
             InputSize = 5,
-            OutputSize = 25
+            OutputSize = 25,
+            Noise = 0.01,
+            MinSongLength = 90,
         },
         new DailyMixConfig
         {
             Name = "Discover",
             SampleMethod = SampleMethod.RandomSongs,
             InputSize = 2,
-            OutputSize = 30
+            OutputSize = 30,
+            Noise = 0.01,
+            MinSongLength = 90,
         },
         new DailyMixConfig
         {
             Name = "Artist Radio",
             SampleMethod = SampleMethod.RandomArtist,
             InputSize = 30,
-            OutputSize = 40
+            OutputSize = 40,
+            Noise = 0.01,
+            MinSongLength = 90,
         },
     ];
 }
